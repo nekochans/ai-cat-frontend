c2860341f2f09bdcd3fd242ce3498942702b728a
keitakn <keita.koga.work@gmail.com>
2023-06-04 15:58:51 +0900

    :wrench: #3 viewport の設定を追加

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import '../src/app/globals.css';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
@@ -9,6 +10,9 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
     },
   },
 };
