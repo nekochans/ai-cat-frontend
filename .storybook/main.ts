9624bdf5f64917e7441a9ff5fa5f27eaaeca63f9
keitakn <keita.koga.work@gmail.com>
2023-06-05 00:22:09 +0900

    :heavy_plus_sign: #3 msw と msw-storybook-addon を利用出来る設定を追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -22,6 +22,7 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag',
   },
+  staticDirs: ['../public'],
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,11 @@
 import '../src/app/globals.css';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import type { Preview } from '@storybook/react';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+initialize();
+
+export const decorators = [mswDecorator];
 
 const preview: Preview = {
   parameters: {
