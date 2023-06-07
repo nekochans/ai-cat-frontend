97682c89618670dc648ab0fe2da416874caeabc5
keitakn <keita.koga.work@gmail.com>
2023-06-07 23:01:10 +0900

    :white_check_mark: #5 Jestの動作確認の為、簡単なテストコードを追加

---
+++ src/app/chat/_components/ChatContent/__tests__/ChatHeader.spec.tsx
@@ -0,0 +1,12 @@
+import { ChatHeader } from '@/app/chat/_components/ChatContent/ChatHeader';
+import { render, screen } from '@testing-library/react';
+
+describe('src/app/chat/_components/ChatContent/ChatHeader.tsx TestCases', () => {
+  it('should display ChatHeader', () => {
+    const expected = 'もこちゃん';
+
+    render(<ChatHeader />);
+
+    expect(screen.getByText(expected)).toBeTruthy();
+  });
+});
