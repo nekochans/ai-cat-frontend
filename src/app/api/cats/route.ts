f268567bc576c9157233c70d7cc2f59df623bd17
keitakn <keita.koga.work@gmail.com>
2023-06-16 13:17:52 +0900

    :sparkles: #28 @upstash/ratelimit でレートリミットを設定

--- src/app/api/cats/route.ts
+++ src/app/api/cats/route.ts
@@ -1,4 +1,6 @@
-import { NextResponse } from 'next/server';
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
+import { NextResponse, type NextRequest } from 'next/server';
 
 type RequestBody = {
   catName: string;
@@ -9,9 +11,36 @@ type ResponseBody = {
   message: string;
 };
 
+const redis = new Redis({
+  url: String(process.env.UPSTASH_REDIS_REST_URL),
+  token: String(process.env.UPSTASH_REDIS_REST_TOKEN),
+});
+
+const rateLimit = new Ratelimit({
+  redis,
+  analytics: true,
+  limiter: Ratelimit.slidingWindow(5, '10 s'),
+  prefix: '@upstash/ratelimit',
+});
+
 export const runtime = 'edge';
 
-export async function POST(request: Request): Promise<NextResponse> {
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const { success } = await rateLimit.limit(
+    request.ip != null ? request.ip : 'anonymous'
+  );
+
+  if (!success) {
+    const responseBody = {
+      type: 'TOO_MANY_REQUESTS',
+      title: 'Too many requests.',
+      detail:
+        'Too many requests from this IP. Please try again after some time.',
+    };
+
+    return NextResponse.json(responseBody, { status: 429 });
+  }
+
   const requestBody = (await request.json()) as RequestBody;
 
   const response = await fetch(
