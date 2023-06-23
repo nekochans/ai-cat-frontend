d70c5c4f62984daff790b19fe351199ee67cfd20
keitakn <keita.koga.work@gmail.com>
2023-06-24 00:19:24 +0900

    :art: #33 送信のショートカットキーを変更したので合わせて placeholder を変更

--- src/app/chat/_components/ChatContent/ChatContent.tsx
+++ src/app/chat/_components/ChatContent/ChatContent.tsx
@@ -115,7 +115,7 @@ export const ChatContent = ({
             <textarea
               id="message-input"
               name="message-input"
-              placeholder="Type your message here. Press Enter + Shift to send."
+              placeholder="Type your message here. Press Command + Enter or Control + Enter to send."
               className="w-full rounded-md py-3 pl-4 text-gray-600 placeholder:text-gray-600  focus:outline-none focus:placeholder:text-gray-400"
               ref={ref}
               onKeyDown={handleKeyDown}
