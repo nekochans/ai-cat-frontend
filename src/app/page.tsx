a53f60182809fdc5ed16e2d4a903f4c4b1abaab0
keitakn <keita.koga.work@gmail.com>
2023-06-28 00:15:17 +0900

    :recycle: #6 ページ下部にあった白い余白を削除するようにCSSを調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ const IndexPage: NextPage = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="bg-yellow-100">
+    <main className="flex min-h-screen flex-col bg-yellow-100">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
           className="flex items-center justify-between p-6 lg:px-8"
@@ -172,7 +172,7 @@ const IndexPage: NextPage = () => {
         </div>
       </div>
       <Footer />
-    </div>
+    </main>
   );
 };
 
