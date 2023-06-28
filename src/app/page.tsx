65a134253bc1db8077adec7b70077a53e309949c
keitakn <keita.koga.work@gmail.com>
2023-06-28 23:27:43 +0900

    :recycle: #6 トップのボタン風のリンクの色をChatのボタンに合わせて変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -151,7 +151,7 @@ const IndexPage: NextPage = () => {
                 <Link
                   href="/chat"
                   prefetch={false}
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   はじめる
                 </Link>
