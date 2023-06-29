ed53f8c355beaa570a16375bd58072903f0065a5
keitakn <keita.koga.work@gmail.com>
2023-06-29 17:53:00 +0900

    :recycle: #6 ハンバーガーメニューの中身の背景色を変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ const IndexPage: NextPage = () => {
           onClose={setMobileMenuOpen}
         >
           <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-yellow-100 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <Link href="/" prefetch={false} passHref className="-m-1.5 p-1.5">
                 {/* TODO 後でちゃんとしたロゴに置き換える */}
