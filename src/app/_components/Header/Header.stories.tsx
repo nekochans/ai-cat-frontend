be47a544c11bfd3d91bf7739ed14e8c59660c729
keitakn <keita.koga.work@gmail.com>
2023-06-30 16:16:11 +0900

    :recycle: #6 Header部分をComponentに分離した

---
+++ src/app/_components/Header/Header.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Header } from './Header';
+
+const meta: Meta = {
+  component: Header,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {};
