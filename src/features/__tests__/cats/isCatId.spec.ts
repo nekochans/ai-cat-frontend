1320590eac244baf53cb782a28aa873bd66bf175
keitakn <keita.koga.work@gmail.com>
2023-07-04 17:05:03 +0900

    :white_check_mark: #38 isCatId のテストコードを実装

---
+++ src/features/__tests__/cats/isCatId.spec.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-magic-numbers */
+import { isCatId, type CatId } from '@/features';
+
+describe('src/features/cat.ts isCatId TestCases', () => {
+  type TestTable = {
+    catId: CatId;
+    expected: boolean;
+  };
+
+  it.each`
+    catId
+    ${'moko'}
+  `('should return true. catId: $catId', ({ catId }: TestTable) => {
+    expect(isCatId(catId)).toBeTruthy();
+  });
+
+  it.each`
+    catId
+    ${'cat'}
+    ${'unknown'}
+  `('should return false. catId: $catId', ({ catId }: TestTable) => {
+    expect(isCatId(catId)).toBeFalsy();
+  });
+});
