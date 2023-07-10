1c130c6c31e60e29dd133a3d76b491d7763c0883
keitakn <keita.koga.work@gmail.com>
2023-07-10 23:09:19 +0900

    :art: #41 ページのデザイン、文言を調整

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,21 +9,21 @@ const NotFound = (): JSX.Element => {
       <Header enableLoginLink={false} />
       <main className="mx-auto w-full max-w-7xl px-6 pb-16 pt-10 sm:pb-24 lg:px-8">
         <div className="mx-auto mt-20 max-w-2xl text-center sm:mt-24">
-          <p className="text-base font-semibold leading-8 text-indigo-600">
+          <p className="text-6xl font-semibold leading-8 text-orange-500">
             404
           </p>
           <Image
             src="/404.webp"
             width={300}
             height={300}
-            alt=""
+            alt="Cat in a Box"
             className="mx-auto"
           />
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-            This page does not exist
+            Page not found
           </h1>
           <p className="mt-4 text-base leading-7 text-gray-600 sm:mt-6 sm:text-lg sm:leading-8">
-            Sorry, we couldn’t find the page you’re looking for.
+            Sorry, we couldn’t find the page you’re looking for😿
           </p>
         </div>
         <div className="mx-auto mt-16 flow-root max-w-lg sm:mt-20">
@@ -31,10 +31,10 @@ const NotFound = (): JSX.Element => {
           <div className="mt-10 flex justify-center">
             <Link
               href="/"
-              className="text-sm font-semibold leading-6 text-indigo-600"
+              className="text-sm font-semibold leading-6 text-orange-500 hover:text-orange-400"
+              prefetch={false}
             >
-              <span aria-hidden="true">&larr;</span>
-              Back to home
+              <span aria-hidden="true">&larr;Back to Top</span>
             </Link>
           </div>
         </div>
