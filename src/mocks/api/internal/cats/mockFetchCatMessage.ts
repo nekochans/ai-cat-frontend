c85355406cd3a3acd2d3c7986c0a964e7210586b
keitakn <keita.koga.work@gmail.com>
2023-10-27 16:19:23 +0900

    :green_heart: #67 やはり実際の動きに近づける為にsleepを使うように変更

--- src/mocks/api/internal/cats/mockFetchCatMessage.ts
+++ src/mocks/api/internal/cats/mockFetchCatMessage.ts
@@ -1,25 +1,35 @@
+import { sleep } from '@/utils';
 import { HttpResponse, type ResponseResolver } from 'msw';
 
 const encoder = new TextEncoder();
 
-export const mockFetchCatMessage: ResponseResolver = async () => {
+export const mockFetchCatMessage: ResponseResolver = () => {
   const stream = new ReadableStream({
-    start(controller) {
+    start: async (controller) => {
       controller.enqueue(
         encoder.encode(
           'data: {"conversationId": "7fe730ac-5ea9-d01d-0629-568b21f72982", "message": "こんにちは🐱"}',
         ),
       );
+
+      await sleep(0.5);
+
       controller.enqueue(
         encoder.encode(
           'data: {"conversationId": "7fe730ac-5ea9-d01d-0629-568b21f72982", "message": "もこだにゃん🐱"}',
         ),
       );
+
+      await sleep(0.5);
+
       controller.enqueue(
         encoder.encode(
           'data: {"conversationId": "7fe730ac-5ea9-d01d-0629-568b21f72982", "message": "お話しようにゃん🐱"}',
         ),
       );
+
+      await sleep(0.5);
+
       controller.enqueue(
         encoder.encode(
           'data: {"conversationId": "7fe730ac-5ea9-d01d-0629-568b21f72982", "message": "🐱🐱🐱"}',
