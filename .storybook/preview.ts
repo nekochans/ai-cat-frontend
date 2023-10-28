c7f830da32e3f59d0ca17679a02368a1ef2c431b
keitakn <keita.koga.work@gmail.com>
2023-10-29 01:54:47 +0900

    :arrow_up: #67 msw-storybook-addon を 2.0.0--canary.122.b3ed3b1.0 に変更
    https://github.com/mswjs/msw-storybook-addon/issues/121#issuecomment-1777192208

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,11 @@
 import '../src/app/globals.css';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import type { Preview } from '@storybook/react';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
 
-// TODO msw-storybook-addon がまだ対応していないので対応したら修正する
-// import { initialize, mswDecorator } from 'msw-storybook-addon';
-//
-// initialize();
-//
-// export const decorators = [mswDecorator];
+initialize();
+
+export const decorators = [mswDecorator];
 
 const preview: Preview = {
   parameters: {
