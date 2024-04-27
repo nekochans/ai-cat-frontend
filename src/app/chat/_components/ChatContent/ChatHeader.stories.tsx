13c8655ea5d6dc18fc69550f8b76fbe5e91d4802
keitakn <keita.koga.work@gmail.com>
2024-04-27 22:33:28 +0900

    :recycle: #101 @testing-library/react をアップグレードしたら型定義エラーになったのでテストをStorybook内に移動

--- src/app/chat/_components/ChatContent/ChatHeader.stories.tsx
+++ src/app/chat/_components/ChatContent/ChatHeader.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { expect, within } from '@storybook/test';
 import { ChatHeader } from './ChatHeader';
 
 const meta: Meta = {
@@ -9,4 +10,16 @@ export default meta;
 
 type Story = StoryObj<typeof ChatHeader>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const expectedCatName = 'もこちゃん';
+    const expectedCatType = 'チンチラシルバー';
+
+    const canvas = within(canvasElement);
+    const header = canvas.getByRole('img', { name: expectedCatName });
+    await expect(header).toHaveAttribute('alt', expectedCatName);
+
+    await expect(canvas.getByText(expectedCatName)).toBeInTheDocument();
+    await expect(canvas.getByText(expectedCatType)).toBeInTheDocument();
+  },
+};
